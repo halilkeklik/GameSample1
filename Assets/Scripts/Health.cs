8459a0a5526fe4728bb01c0c61ef3219cf5f46b6
halilkeklik <halil@halilkeklik.com>
2023-02-01 19:28:09 +0300

    Added player and enemy health bar.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public float health;
     public float maxHealth;
+    public Image healthBar;
     
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
     }
 
     public void TakeDamage(float damage)
@@ -21,6 +29,7 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+            healthBar.fillAmount = 0f;
         }
     }
 }
