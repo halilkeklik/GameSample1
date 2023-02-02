8bb7b6f8683b33ddada27f1fd0a81c749f8e77ae
halilkeklik <halil@halilkeklik.com>
2023-02-02 18:11:15 +0300

    Change enemy combat.

--- Assets/Scripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyCombat.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class EnemyCombat : MonoBehaviour
 {
-    [SerializeField] private Health playerHealth;
     [SerializeField] private float damage;
-    
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Player")
         {
-            playerHealth.TakeDamage(damage);
+            col.collider.GetComponent<Health>().TakeDamage(damage);
         }
     }
 }
